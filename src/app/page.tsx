528f37d41d4a679d8f06dff3b70df8e23fd74539
davidemilizia <davidemilizia@gmail.com>
2026-09-03 09:22:24 +0200

    Create HomePage component for Gluten Free Finder

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Gluten Free Finder</h1>
+
+      <p>
+        Trova ristoranti, negozi e attività
+        dedicate ai celiaci.
+      </p>
+    </main>
+  );
+}
