12b4b3c8831c814ae386317b2ca0b2eda150b1aa
davidemilizia <davidemilizia@gmail.com>
2026-09-21 10:58:52 +0200

    Add files via upload

---
+++ src/app/places/page.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import places from "../../../data/places.json";
+
+export default function PlacesPage() {
+  return (
+    <main>
+      <h1>Locali Gluten Free</h1>
+
+      {places.map((place: any) => (
+        <div key={place.id}>
+          <h2>
+            <Link href={`/places/${place.slug}`}>
+              {place.name}
+            </Link>
+          </h2>
+
+          <p>{place.address}</p>
+          <p>⭐ {place.rating}/5</p>
+          <p>{place.description}</p>
+          <hr />
+        </div>
+      ))}
+    </main>
+  );
+}
